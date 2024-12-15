b4e4af75f37347d9d30d2d0cd1691eca196c197f
类子哥 <61436725@qq.com>
2024-12-15 21:26:11 +0800

    Add files via upload
    Signed-off-by: 类子哥 <61436725@qq.com>

---
+++ MS.js
@@ -0,0 +1,7 @@
+// MS.js
+const comments = [
+    { id: 1, author: "Alice", message: "Hello, this is my first comment!" },
+    { id: 2, author: "Bob", message: "Great idea!" }
+];
+ 
+module.exports = comments;
